ff3f22a1b930b59fdc7af8a7cad6768304fb23b2
xzxice <73463853+xzxice@users.noreply.github.com>
2020-10-26 11:03:03 +0300

    Code.py
    The main code.

---
+++ code.py
@@ -0,0 +1,6 @@
+## Input:
+A_string = "Hello," ## String (named A_string)
+B_string = "World!" ## String (named B_string)
+print(A_string + " " + B_string) ## Print (A_string, " ", B_string
+
+## Output: Hello, World!
